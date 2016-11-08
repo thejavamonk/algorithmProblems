752bcaa00140d725ca408a97abb08e2ed89d2de0
praveen <thejavamonk@gmail.com>
2016-11-08 14:58:41 +0530

    solved camelCase

---
+++ algorithms_warmup/src/com/codeprep/camelcase/Solution.java
@@ -0,0 +1,24 @@
+package com.codeprep.camelcase;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int capsCount = 0;
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		char[] charArray = s.toCharArray();
+		for (char c : charArray) {
+			if (Character.isUpperCase(c)) {
+				capsCount++;
+			}
+		}
+		if (capsCount > 0) {
+			System.out.println(capsCount + 1);
+		}else{
+			System.out.println(1);
+		}
+	}
+
+}

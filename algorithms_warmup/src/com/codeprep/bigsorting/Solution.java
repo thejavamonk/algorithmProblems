789c79c095dc6e6fa1c3a51032cc83e8fb5089a8
thejavamonk <thejavamonk@gmail.com>
2017-11-19 19:18:21 +0530

    big sorting problem solution

--- algorithms_warmup/src/com/codeprep/bigsorting/Solution.java
+++ algorithms_warmup/src/com/codeprep/bigsorting/Solution.java
@@ -1,17 +1,39 @@
 package com.codeprep.bigsorting;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Solution {
 	
 	public static void main(String[] args) {
 		
-		Scanner in = new Scanner(System.in);
-	    int sizeOfArray = in.nextInt();
-	    int arr[] = new int[sizeOfArray];
-	    for(int arr_i=0; arr_i < sizeOfArray; arr_i++){
-	        arr[arr_i] = in.nextInt();
-	    }
+		BigInteger temp = new BigInteger("0");
+    	BigInteger indexOfSmallest = new BigInteger("0");
+        Scanner in = new Scanner(System.in);
+        int sizeOfArray = Integer.parseInt(in.next());
+        BigInteger arr[] = new BigInteger[sizeOfArray];
+        for(int arr_i=0; arr_i < sizeOfArray; arr_i++){
+            arr[arr_i] = new BigInteger(in.next());
+        }
+        
+        for(int i=0; i < sizeOfArray; i++){
+        	BigInteger small = arr[i];
+        	indexOfSmallest = BigInteger.valueOf(new Long(i));
+        	for (int j = i + 1; j < sizeOfArray; j++){
+        		if(small.compareTo(arr[j]) > 0){
+        			indexOfSmallest = BigInteger.valueOf(new Long(j));
+        			small = arr[j];
+        		}
+        	}
+        	if(i != indexOfSmallest.intValue()){
+        		temp = arr[i];
+            	arr[i] = small;
+            	arr[indexOfSmallest.intValue()] = temp;
+        	}
+        }
+        for (BigInteger i : arr) {
+        	System.out.println(i);
+		}
 	    
 	    
 	}

ca500691d36642269302c67cb6839f4036087478
thejavamonk <thejavamonk@gmail.com>
2017-11-20 23:18:16 +0530

    solution for insertion sort 1

--- algorithms_warmup/src/com/codeprep/insertionsort1/Solution.java
+++ algorithms_warmup/src/com/codeprep/insertionsort1/Solution.java
@@ -6,37 +6,48 @@ public class Solution {
 
 	public static void main(String[] args) {
 		
-		String elements = null;
 		Scanner scanner = new Scanner(System.in);
 		int lengthOfTheArray = Integer.parseInt(scanner.next());
+		int[] intArray = new int[lengthOfTheArray];
 		for (int i = 0; i < lengthOfTheArray; i++) {
-			elements = elements +" "+ scanner.next();
+			intArray[i] = Integer.parseInt(scanner.next());
 		}
+		
 		scanner.close();
 
-		String[] array = elements.split(" ");
-		int[] intArray = new int[lengthOfTheArray];
-		for (int i = 0; i < intArray.length; i++) {
-			intArray[i] = Integer.parseInt(array[i]);
-		}
 		determinePosition(intArray);
 	}
 
 	private static void determinePosition(int[] testArray) {
-		int lastValue = testArray[testArray.length];
-		int swap = testArray[testArray.length - 1];
-		testArray[testArray.length] = swap;
-		int start = testArray.length - 1;
-		for (int i = start ; i < 0; i--) {
-			if(testArray[start] > lastValue){
-				testArray[start + 1] = testArray[start];
-				System.out.println(testArray.toString());
+		
+		int lastValue = testArray[testArray.length - 1];
+		for (int i = testArray.length - 2 ; i < testArray.length; i--) {
+			if(i == -1){
+				testArray[0] = lastValue;
+				printArray(testArray);
+				break;
+			}
+			if(testArray[i] > lastValue){
+				testArray[i + 1] = testArray[i];
+				printArray(testArray);
 			}else{
-				testArray[start + 1] = lastValue;
-				System.out.println(testArray.toString());
+				testArray[i + 1] = lastValue;
+				printArray(testArray);
 				break;
 			}
 		}
 	}
-
+	
+	private static void printArray(int[] intArray){
+		
+		String output = null;
+		for (int i : intArray) {
+			if(output != null){
+				output = output +" "+ new Integer(i).toString();
+			}else{
+				output = new Integer(i).toString();
+			}
+		}
+		System.out.println(output);
+	}
 }

a7995edbf30f33e3cd96b9e34cf6f2feae72d395
praveen <thejavamonk@gmail.com>
2020-07-23 17:51:19 +0530

    solution to array left rotation problem

---
+++ arrays/src/main/java/com/codeprep/leftrotation/Solution.java
@@ -0,0 +1,49 @@
+package com.codeprep.leftrotation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+
+    	int n = a.length;
+    	int[] rotarr = new int [a.length];
+    	for(int oldindex = 0; oldindex < a.length; oldindex++) {
+    		int newIndex = (oldindex + n - d) % n;
+    		rotarr[newIndex] = a[oldindex];
+    	}
+    	return rotarr;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+        System.out.println(result);
+        scanner.close();
+    }
+}
+

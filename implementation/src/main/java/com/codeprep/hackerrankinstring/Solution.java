cfc043959d52de33095317356938fdda1d630199
praveen <thejavamonk@gmail.com>
2020-09-30 17:46:12 +0530

    solution to hackerrank in string problem

---
+++ implementation/src/main/java/com/codeprep/hackerrankinstring/Solution.java
@@ -0,0 +1,39 @@
+package com.codeprep.hackerrankinstring;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+	// Complete the hackerrankInString function below.
+	static String hackerrankInString(String s) {
+
+        String str = "hackerrank";
+        if (s.length() < str.length()) {
+            return "NO";
+        }
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (j < str.length() && s.charAt(i) == str.charAt(j)) {
+                    j++;
+            }
+        }
+        return (j == str.length() ? "YES" : "NO");
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		int q = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int qItr = 0; qItr < q; qItr++) {
+			String s = scanner.nextLine();
+
+			String result = hackerrankInString(s);
+
+			System.out.println(result);
+		}
+		scanner.close();
+	}
+}

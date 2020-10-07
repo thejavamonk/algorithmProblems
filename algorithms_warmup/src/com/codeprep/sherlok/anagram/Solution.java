233cc3141ce5f65e27fa8fb04ef822c539780f3d
praveen <thejavamonk@gmail.com>
2020-10-07 15:06:27 +0530

    solution to sherlock and anagram problem

--- algorithms_warmup/src/com/codeprep/sherlok/anagram/Solution.java
+++ algorithms_warmup/src/com/codeprep/sherlok/anagram/Solution.java
@@ -1,53 +1,50 @@
 package com.codeprep.sherlok.anagram;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Solution {
 
-    // Complete the sherlockAndAnagrams function below.
-    static int sherlockAndAnagrams(String s) {
+	// Complete the sherlockAndAnagrams function below.
+	static int sherlockAndAnagrams(String s) {
 
-        HashMap<String, Integer> map= new HashMap<>(); 
-  
-        for(int i = 0; i < s.length(); i++){ 
-            for(int j = i; j < s.length(); j++){ 
-                char[] valC = s.substring(i, j+1).toCharArray(); 
-                Arrays.sort(valC); 
-                String val = new String(valC); 
-                if (map.containsKey(val))  
-                    map.put(val, map.get(val)+1); 
-                else 
-                    map.put(val, 1); 
-            } 
-        } 
-        int anagramPairCount = 0; 
-        for(String key: map.keySet()){ 
-            int n = map.get(key); 
-            anagramPairCount += (n * (n-1))/2; 
-        } 
-        return anagramPairCount; 
-    }
+		HashMap<String, Integer> map = new HashMap<>();
 
-    private static final Scanner scanner = new Scanner(System.in);
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i; j < s.length(); j++) {
+				char[] valC = s.substring(i, j + 1).toCharArray();
+				Arrays.sort(valC);
+				String val = new String(valC);
+				if (map.containsKey(val))
+					map.put(val, map.get(val) + 1);
+				else
+					map.put(val, 1);
+			}
+		}
+		int anagramPairCount = 0;
+		for (String key : map.keySet()) {
+			int n = map.get(key);
+			anagramPairCount += (n * (n - 1)) / 2;
+		}
+		return anagramPairCount;
+	}
 
-    public static void main(String[] args) throws IOException {
+	private static final Scanner scanner = new Scanner(System.in);
 
-        int q = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	public static void main(String[] args) throws IOException {
 
-        for (int qItr = 0; qItr < q; qItr++) {
-            String s = scanner.nextLine();
+		int q = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-            int result = sherlockAndAnagrams(s);
+		for (int qItr = 0; qItr < q; qItr++) {
+			String s = scanner.nextLine();
 
-            System.out.println(String.valueOf(result));
-        }
-        scanner.close();
-    }
+			int result = sherlockAndAnagrams(s);
+
+			System.out.println(String.valueOf(result));
+		}
+		scanner.close();
+	}
 }

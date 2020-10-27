093129817782e7a6e663722de3d14af00d0caeca
praveen <thejavamonk@gmail.com>
2020-10-27 14:47:58 +0530

    solution to diagonal difference problem

--- algorithms_warmup/src/com/codeprep/diagonaldifference/Solution.java
+++ algorithms_warmup/src/com/codeprep/diagonaldifference/Solution.java
@@ -1,29 +1,60 @@
 package com.codeprep.diagonaldifference;
 
-import java.util.Scanner;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class Result {
+
+	/*
+	 * Complete the 'diagonalDifference' function below.
+	 *
+	 * The function is expected to return an INTEGER. The function accepts
+	 * 2D_INTEGER_ARRAY arr as parameter.
+	 */
+
+	public static int diagonalDifference(List<List<Integer>> arr) {
+
+		int sum1 = 0;
+		int sum2 = 0;
+
+		int stIndex = 0;
+		int lstIndex = 1;
+		for (List<Integer> row : arr) {
+
+			sum1 = sum1 + row.get(stIndex);
+			sum2 = sum2 + row.get(row.size() - lstIndex);
+			stIndex++;
+			lstIndex++;
+		}
+		return Math.abs(sum1 - sum2);
+	}
+}
 
 public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        
-        Scanner in = new Scanner(System.in);
-        int sizeOfMatrix = in.nextInt();
-        int a[][] = new int[sizeOfMatrix][sizeOfMatrix];
-        for(int a_i=0; a_i < sizeOfMatrix; a_i++){
-            for(int a_j=0; a_j < sizeOfMatrix; a_j++){
-                a[a_i][a_j] = in.nextInt();
-            }
-        }
-        int sumOfRight = 0;
-        int sumOfLeft = 0;
-        
-        for(int i =0; i < sizeOfMatrix; i++){
-            sumOfRight = sumOfRight + a[i][i];
-        }
-        for(int i =0, j = sizeOfMatrix - 1; i <sizeOfMatrix; i++, j--){
-            sumOfLeft = sumOfLeft + a[j][i];
-        }
-        int sum = sumOfRight - sumOfLeft;
-        System.out.println(sum);
-    }
+		int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+		List<List<Integer>> arr = new ArrayList<>();
+
+		IntStream.range(0, n).forEach(i -> {
+			try {
+				arr.add(Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt)
+						.collect(toList()));
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		});
+
+		int result = Result.diagonalDifference(arr);
+		System.out.println(String.valueOf(result));
+	}
 }

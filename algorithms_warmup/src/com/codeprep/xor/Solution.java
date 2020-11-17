81da9d2a63ab59a1cddb60c17ab41c1910382cb1
praveen <thejavamonk@gmail.com>
2020-11-17 17:38:46 +0530

    Solution to XOR debugging problem

---
+++ algorithms_warmup/src/com/codeprep/xor/Solution.java
@@ -0,0 +1,27 @@
+package com.codeprep.xor;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static String stringsXOR(String s, String t) {
+		StringBuilder res = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == t.charAt(i))
+				res.append("0");
+			else
+				res.append("1");
+		}
+		return res.toString();
+	}
+
+	public static void main(String[] args) {
+
+		String s, t;
+		Scanner in = new Scanner(System.in);
+		s = in.nextLine();
+		t = in.nextLine();
+		System.out.println(stringsXOR(s, t));
+
+	}
+}
